5b8e173083f513a46088866bdcf116b38a856c40
Kevin ricci <kevinrh.dev@gmail.com>
2019-04-11 11:35:25 -0300

    Obtendo um usuário por id

--- src/main/java/com/kevinrh/workshopmongo/services/UserService.java
+++ src/main/java/com/kevinrh/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.kevinrh.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kevinrh.workshopmongo.domain.User;
 import com.kevinrh.workshopmongo.repository.UserRepository;
+import com.kevinrh.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		List<User> list = repository.findAll();
 		return list;
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/kevinrh/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.kevinrh.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
